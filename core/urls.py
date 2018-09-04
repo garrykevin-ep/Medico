0d1947fcc727265ef460032b4546f733ce1dce26
garrykevin <garrykevin.ep@gmail.com>
2018-09-04 20:49:57 +0530

    added

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^diaflow/',views.diaflow)
+    url(r'^diaflow/',views.diaflow),
+    url(r'^stock/', views.stock_list)
 ]
--- core/views.py
+++ core/views.py
@@ -1,29 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from .models import *
 # Create your views here.
-
-JSON_PAYLOAD={
-  "payload": {
-    "google": {
-      "expectUserResponse": True,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "this is a simple response from web hook"
-            }
-          }
-        ]
-      }
-    }
-  }
-}
 
 
 def diaflow(request):
-	# if request.method == 'POST':	
 	res = "this is a simple response from web hook"
-	return JsonResponse({'fulfillmentText': res})
+	action = request.POST['queryResult']['action']
+	return JsonResponse({'fulfillmentText': action})
 	return JsonResponse(JSON_PAYLOAD)
-	return HttpResponse('hi')
+
+def all_stock():
+	medicines = Medicine.objects.all()
+	response = 'you have '
+	last_index = len(medicines)-1
+	for index,medicine in enumerate(medicines):
+		response += str(medicine.quantity) + ' ' + medicine.name
+		if index != last_index:
+			response += ' and '
+	return response
+
+def stock_list(request):
+	'''
+	returns a json list of all medicines
+	'''
+	medicines = Medicine.objects.all()
+	response = []
+	for medicine in medicines:
+		json_obj = {
+		'name' : medicine.name,
+		'quantity' : medicine.quantity
+		}
+		response.append(json_obj)
+	return JsonResponse(response,safe=False)
