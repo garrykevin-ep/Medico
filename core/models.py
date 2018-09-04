7c00d011801e2660569bf3e630f4fe554f6fefc2
garrykevin <garrykevin.ep@gmail.com>
2018-09-04 11:00:30 +0530

    make it better

---
+++ core/models.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Medicine(models.Model):
+	name = models.CharField(max_length=50)
+	quantity = models.IntegerField()
+
+	def __str__(self):
+		return self.name
+
+class Order(models.Model):
+	user = models.ForeignKey(User,on_delete = models.CASCADE)
+	medicines = models.ManyToManyField(Medicine)
+	
+
+class OrderDetails(models.Model):
+	order = models.ForeignKey(Order,on_delete = models.CASCADE)
+	medicine = models.ForeignKey(Medicine,on_delete = models.CASCADE) 
+	quantity = models.IntegerField()
