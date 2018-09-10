eba86b394dd3aa1f7983814a3482e167edd07807
garrykevin <garrykevin.ep@gmail.com>
2018-09-10 19:10:32 +0530

    local

--- core/admin.py
+++ core/admin.py
@@ -3,5 +3,25 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+class OrderDetailInline(admin.TabularInline):
+    model = OrderDetail
+    extra = 3
+ 
+
+class OrderAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Order
+    '''
+    # list_display = ('',)
+    list_filter = ('approved',)
+    inlines = [
+        OrderDetailInline,
+    ]
+    # raw_id_fields = ('',)
+    # readonly_fields = ('',)
+    # search_fields = ('',)
+
+admin.site.register(Order, OrderAdmin)
+
 admin.site.register(Medicine)
-admin.site.register(Order)
+# admin.site.register(Order)	
